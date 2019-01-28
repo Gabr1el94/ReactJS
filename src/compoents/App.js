6a52c12e6aca8999e4b440f9d2be7dc5e1fc99c4
Gabriel Soares <bielsystem64@gmail.com>
2019-01-28 16:19:40 -0300

    increment and decrement operating with sucessfull!

---
+++ src/compoents/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contador from './Contador';
+//Create the class first
+export default class App extends React.Component{
+
+    //return 
+    render(){
+        return(
+                 <Contador />
+        );
+    
+    }
+}
