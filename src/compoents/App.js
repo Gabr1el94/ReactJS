e0f7e1c6684144edefac76a803c9cd7264cd207b
Gabriel Soares <bielsystem64@gmail.com>
2019-01-29 16:57:15 -0300

    Data's with format in the json inside object

--- src/compoents/App.js
+++ src/compoents/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Placar from './Placar';
+
+const dados = {
+    partida:{
+        estadio:"Ilha do Retiro/PE",
+        data:"01/06/2016",
+        horario:"19h"
+    },
+    casa:{
+        nome:"Sport"
+    },
+    visitante:{
+        nome:"Santa Cruz"
+    }
+};
+
 //Create the class first
 export default class App extends React.Component{
 
     //return 
     render(){
         return(
-                 <Placar />
+                 <Placar {...dados} />
         );
     
     }
