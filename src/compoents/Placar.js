190f8e63c7d5a12ca3473b799147e3d81da9e226
Gabriel Soares <bielsystem64@gmail.com>
2019-01-29 17:04:16 -0300

    Structure of  "Placar"

--- src/compoents/Placar.js
+++ src/compoents/Placar.js
@@ -8,23 +8,50 @@ export default class PlacarContainer extends React.Component {
     constructor() {
         super();
 
+        this.state={
+            gols_casa:0,
+            gols_visitante:0,
+        }
+    }
+
+    marcarGolCasa(){
+        this.setState({
+            gols_casa: this.state.gols_casa + 1,
+        });
+    }
+
+    marcarGolVisitante(){
+        this.setState({
+            gols_visitante: this.state.gols_visitante + 1,
+        });
     }
 
     render() {
+        //extraction data's
+        const {partida,casa,visitante} = this.props;
+        const estilo = {
+            float: "left",
+            "marginRight":"20px",
+        }
+        
         return (
             <div>
-                <div style={{ float: "left", "marginRight": "10px" }}>
+                <div style={estilo}>
                     <h3>Casa</h3>
-                    <Time />
+                    <Time nome={casa.nome} gols={this.state.gols_casa}
+                        marcarGol={this.marcarGolCasa.bind(this)}
+                    />
                 </div>
 
-                <div style={{ float: "left" }}>
-                    <Partida />
+                <div style={estilo}>
+                    <Partida {...partida}
+                    />
                 </div>
 
-                <div style={{ float: "left" }}>
+                <div style={estilo}>
                     <h3>Visitante</h3>
-                    <Time />
+                    <Time nome={visitante.nome} gols={this.state.gols_visitante} 
+                    marcarGol={this.marcarGolVisitante.bind(this)}/>
                 </div>
 
                 <div style={{clear:"both"}}></div>
@@ -32,5 +59,5 @@ export default class PlacarContainer extends React.Component {
             </div>
         );
     }
-
 }
+
